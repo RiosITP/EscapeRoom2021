a2e44afb38ac720c1e13e7c93c65d81e97d06492
RiosITP <43545189+RiosITP@users.noreply.github.com>
2021-03-01 09:30:18 -0500

    Update server.js
    Added some comments and cleaned up some extraneous lines of code

--- SocketChatAndPlayerData/server.js
+++ SocketChatAndPlayerData/server.js
@@ -2,111 +2,103 @@
 // where your node app starts
 
 // init project
-const express = require("express");
+const express = require("express"); 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const socket = require('socket.io');
+const socket = require("socket.io"); 
 
-const INDEX = 'views';
+const INDEX = "views";
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 let players = [];
 
-// // http://expressjs.com/en/starter/static-files.html
-// app.use(express.static("public"));
-
-//http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-
 //listen for requests :)
-// const listener = app.listen(process.env.PORT, function() {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
-
-const listener = app.listen(3000, function() {
+const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
 const io = socket(listener);
 
-io.sockets.on('connect', (socket) => {
-    console.log("a new connection! from: " + socket.id + " (server side)")
+// socket connection
+io.on("connect", socket => { 
+  console.log("a new connection! from: " + socket.id + " (server side)");
 
-    socket.on('disconnect', () => {
-      let updtdclients = io.sockets.clients();
-      let connectedIds = Object.keys(updtdclients.connected);
-      
-      if(players.length > 0){
-        console.log("check against the players")
-        for(let i =0 ;i< players.length; i++){
-          let dcontd = true;
-          console.log("player "+i);
-          for(let j=0; j< connectedIds.length ;j++){
-            console.log("check connections")
-            if(players[i].id == connectedIds[j]){
-              dcontd = false;
-            }
+  //disconnected socket
+  socket.on("disconnect", () => {
+    //  let updtdclients = io.sockets.clients();
+    let updtdclients = io.sockets.sockets;
+    //console.log(updtdclients);
+    let connectedds = updtdclients.keys();
+    let connectedIds = Array.from(connectedds);
+    //console.log("connected ids:");
+    //console.log(connectedIds); //map iterator
+
+    if (players.length > 0) {
+      //console.log("check against the players");
+      for (let i = 0; i < players.length; i++) {
+        let dcontd = true;
+        //console.log("player " + i);
+        for (let j = 0; j < connectedIds.length; j++) {
+          // console.log("check connections");
+          if (players[i].id == connectedIds[j]) {
+            dcontd = false;
           }
-          if(dcontd){
-            console.log( players[i].name +" with id: "+players[i].id + " has disconnected")
-           // players.splice(players[i],1);
-            players.splice(i,1);
-            updater();
-          }
-          
-        
-        console.log(players);
-         //socket.emit('updatePlayers', players);
-         // socket.broadcast.emit('updatePlayers', players);
-        } 
-      }
-    //  console.log( socket.id+ " has disconnected: "); 
-    });
-
-    socket.on('begingame', (data) => {
-      let found = false;
-      for(let player in players){
-        if(player.id == socket.id){
-          found = true;
         }
-      }
-      if(!found){
-        players.push(data);
-      }
-      console.log(players);
-      io.emit('updatePlayers', players);
-    });
-
-    socket.on('updatePlayerStatus',(updatedPlayersStatus)=>{
-      players = updatedPlayersStatus;
-    });
-
-    socket.on('msg',(info)=>{
-
-      socket.broadcast.emit('msg', info);
-
-    });
-
-    socket.on('move', (info) => {
-      for(let i = 0; i< players.length ; i++){
-        if(players[i].id == info.id){
-          players[i].x = info.x;
-          players[i].y = info.y;
-          ///console.log("update");
+        if (dcontd) {
+          console.log(players[i].name + " with id: " + players[i].id + " has disconnected");
+          players.splice(i, 1);
+          io.emit("playerLeft", players);
         }
+        //console.log(players);
       }
-      socket.broadcast.emit('movePlayers', players);
-      //console.log(players);
-    });
+    }
+  });
+
+  // when a player clicks to begin
+  socket.on("begingame", data => {
+    // check if they are already in the player array
+    let found = false;
+    for (let i = 0; i < players.length; i++) {
+      //ignore them if they are already in the array
+      if (players[i].id == socket.id) {
+        found = true;
+      }
+    }
+    //if they are not in the array, then add them
+    if (!found) {
+      players.push(data);
+    }
+    // update all clients when someone joins
+    io.emit("updatePlayers", players);
+  });
+  // when a client types a message, update that message in the array
+  socket.on("msg", info => {
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].id == info.sender) {
+        players[i].message = info.message;
+      }
+    }
+    // let every other client know what your message is 
+    socket.broadcast.emit("msg", info);
+  });
+  // when a player moves
+  socket.on("move", info => {
+    // find that player in the array
+    for (let i = 0; i < players.length; i++) {
+      // update their position if they are there
+      if (players[i].id == info.id) {
+        players[i].x = info.x;
+        players[i].y = info.y;
+      }
+    }
+    // let all the other clients know which player moved and where
+    socket.broadcast.emit("movePlayers", info);
+  });
 });
-
-function updater(){
-  io.emit('updatePlayers', players);
-  console.log("sent updated players from a disconnect")
-}
